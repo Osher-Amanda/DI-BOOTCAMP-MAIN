97d9f94b22f52173bed21ee834303c8ac7e48815
Osher-Amanda <osheramandafavel@gmail.com>
2025-09-10 16:33:59 +0300

    Delete day1 directory

--- day1/XPExercise.js/Exercise3.js
+++
@@ -1,7 +0,0 @@
-// Prompt the user for a number
-let number = parseInt(prompt("Enter a number")); // parseInt converts string to number
-
-// While the number is smaller than 10, keep asking
-while (number < 10) {
-  number = parseInt(prompt("Enter a number greater than or equal to 10"));
-}
--- day1/ageCalculator.js/Javascript object checker exercise/practiceExercises.js/PracticeExercises.js
+++
@@ -1,9 +0,0 @@
- for (statement 1; statement 2; statement 3) {
-      // code block to be executed
-    }
-
-    for (let i = 0; i < 2; i++) {
-    console.log("the current number is " + i)
-}
-
-    
